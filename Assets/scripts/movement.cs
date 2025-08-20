b61a26a9a3dd726c4b78dde55720771fcee85f26
jay-tell3 <jaybroww@gmail.com>
2025-08-20 14:54:01 -0500

    movement

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    /*
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, 5);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, -5);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.linearVelocity = new Vector3(5, rb.linearVelocity.y, rb.linearVelocity.z);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.linearVelocity = new Vector3(-5, rb.linearVelocity.y, rb.linearVelocity.z);
+            }
+
+        }
+    */
+    void Update()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDirection += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection -= transform.right;
+        }
+
+        moveDirection.Normalize(); // To prevent faster diagonal movement
+        moveDirection *= 5f; // Speed
+
+        rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.z);
+    }
+    
+}
