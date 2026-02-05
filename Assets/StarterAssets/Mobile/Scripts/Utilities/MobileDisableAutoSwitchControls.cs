15a658a1e375cf03c043192c796f10b987324524
jay-tell3 <jaybroww@gmail.com>
2026-02-05 13:03:39 -0600

    moblie

--- Assets/StarterAssets/Mobile/Scripts/Utilities/MobileDisableAutoSwitchControls.cs
+++ Assets/StarterAssets/Mobile/Scripts/Utilities/MobileDisableAutoSwitchControls.cs
@@ -17,19 +17,13 @@ public class MobileDisableAutoSwitchControls : MonoBehaviour
     
 #if ENABLE_INPUT_SYSTEM && (UNITY_IOS || UNITY_ANDROID)
 
-    [Header("Target")]
-    public PlayerInput playerInput;
-
-    void Start()
-    {
-        DisableAutoSwitchControls();
-    }
-
-    void DisableAutoSwitchControls()
-    {
-        playerInput.neverAutoSwitchControlSchemes = true;
-    }
+    
+#else
+       private void Start()
+{
+   Destroy(this.gameObject);
+}
 
 #endif
-    
+
 }
